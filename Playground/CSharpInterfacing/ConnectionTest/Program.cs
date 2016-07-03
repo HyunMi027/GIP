79b076146d1966289ae89689525b9fffe5fbe779
HyunMi <HyunMi@gmail.com>
2016-07-03 21:49:45 +0200

    Added CSharpInterfacing solution

---
+++ Playground/CSharpInterfacing/ConnectionTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ConnectionTest
+{
+    public static class Program
+    {
+        private static void Main(string[] args)
+        {
+            SerialPort port = new SerialPort("COM3", 9600);
+            port.Open();
+            while (port.IsOpen)
+            {
+                Console.WriteLine(port.ReadLine());
+            }
+        }
+    }
+}
+
+/* Arduino code
+
+    void setup() 
+    {
+        Serial.begin(9600);
+    }
+
+    void loop()
+    {
+        Serial.println(millis());
+    }
+ */
